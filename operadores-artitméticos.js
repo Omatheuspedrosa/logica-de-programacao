6a4cca071ea0abec128d5446704d846fbb233aa8
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-03-14 14:13:46 +0000

    criando o arquivo de operadores aritméticos

---
+++ operadores-artitméticos.js
@@ -0,0 +1,8 @@
+const num1 = 40;
+const num2 = 20;
+
+console.log(`A soma dos números é: ${num1 + num2}`);
+console.log(`A subtração dos números é: ${num1 - num2}`);
+console.log(`A multiplicação dos números é: ${num1 * num2}`);
+console.log(`A divisão dos números é: ${num1 / num2}`);
+console.log(`O resto da divisão dos números é: ${num1 % num2}`);
