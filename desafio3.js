814e8917b40a4e16fee152fbf9bb8c0ad1d4218e
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-03-18 15:20:27 +0000

    parte do desafio3 resolvido

--- desafio3.js
+++ desafio3.js
@@ -1,4 +1,4 @@
-const temperaturaAtual = window.prompt('Temperatura atual: ');
+let temperaturaAtual = window.prompt('Temperatura atual: ');
 const escalaAtual = window.prompt('Escolha a escala atual F, C ou  K');
 const conversao = window.prompt('Escolha a escala a ser convertida F, C ou  K');
 let calculo = 0;
@@ -8,6 +8,10 @@ if (escalaAtual === 'C' && conversao === 'F') {
     calculo = (temperaturaAtual * 9/5) + 32;
     window.alert(`Celsius para Fahrenheit: ${calculo}°F`)
 }else if (escalaAtual === 'C' && conversao === 'K') {
-    calculo = temperaturaAtual + 273;
+    calculo = (temperaturaAtual * 1) + 273;
     window.alert(`Celsius para Kelvin: ${calculo}°K`)
+    window.alert(` ${temperaturaAtual}`)
+}else if (escalaAtual === 'F' && conversao === 'C'){
+    calculo = (temperaturaAtual - 32) * (5/9)
+    window.alert(`Fahrenheit para Celsius: ${calculo}°C`)
 }
