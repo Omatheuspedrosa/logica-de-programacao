dc3cd49f78ca149aec9b17e5cb9e4a8a73d7e3c7
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-03-18 11:22:08 +0000

    concluído o desafio2

--- desafio2.js
+++ desafio2.js
@@ -1,9 +1,28 @@
 const altura = window.prompt("Digite sua altura:");
 const peso = window.prompt("Digite o seu peso:");
-const imc = peso ** altura;
+const calculoImc = peso / altura ** 2;
+const imc = calculoImc.toFixed(2);
 
 if (imc <= 18.5) {
+    window.alert(`IMC: ${imc}`)
     window.alert('Abaixo do peso')
-}else {
-    window.alert('')
+}else if (imc <= 24.9) {
+    window.alert(`IMC: ${imc}`)
+    window.alert('Peso normal')
+}
+else if (imc <= 29.9) {
+    window.alert(`IMC: ${imc}`)
+    window.alert('Sobrepeso')
+}
+else if (imc <= 34.9) {
+    window.alert(`IMC: ${imc}`)
+    window.alert('Obesidade grau I')
+}
+else if (imc <= 39.9) {
+    window.alert(`IMC: ${imc}`)
+    window.alert('Obesidade grau II')
+}
+else {
+    window.alert(`IMC: ${imc}`)
+    window.alert('Obesidade grau III')
 }
