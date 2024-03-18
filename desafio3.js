dbf6f2f9ea65c8901774bd511cbe0ed005a467bc
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-03-18 22:21:25 +0000

    desafio3 concluído

--- desafio3.js
+++ desafio3.js
@@ -1,17 +1,25 @@
 let temperaturaAtual = window.prompt('Temperatura atual: ');
-const escalaAtual = window.prompt('Escolha a escala atual F, C ou  K');
-const conversao = window.prompt('Escolha a escala a ser convertida F, C ou  K');
+const escalaAtual = window.prompt("Escolha a escala atual:\nF - Fahrenheit \nC - Celsius \nK - Kelvin").toUpperCase();
+const conversao = window.prompt('Escolha a escala a ser convertida: \nF - Fahrenheit \nC - Celsius \nK - Kelvin').toUpperCase();
 let calculo = 0;
 
 
 if (escalaAtual === 'C' && conversao === 'F') {
     calculo = (temperaturaAtual * 9/5) + 32;
-    window.alert(`Celsius para Fahrenheit: ${calculo}°F`)
+    window.alert(`${temperaturaAtual}° Celsius para Fahrenheit: ${calculo.toFixed(2)}°F`)
 }else if (escalaAtual === 'C' && conversao === 'K') {
     calculo = (temperaturaAtual * 1) + 273;
-    window.alert(`Celsius para Kelvin: ${calculo}°K`)
-    window.alert(` ${temperaturaAtual}`)
+    window.alert(`${temperaturaAtual}° Celsius para Kelvin: ${calculo.toFixed(2)}°K`);
 }else if (escalaAtual === 'F' && conversao === 'C'){
-    calculo = (temperaturaAtual - 32) * (5/9)
-    window.alert(`Fahrenheit para Celsius: ${calculo}°C`)
+    calculo = (temperaturaAtual - 32) * (5/9);
+    window.alert(`${temperaturaAtual}° Fahrenheit para Celsius: ${calculo.toFixed(2)}°C`);
+}else if (escalaAtual === 'F' && conversao === 'K'){
+    calculo = ((temperaturaAtual * 1) + 459.67) * 5/9;
+    window.alert(`${temperaturaAtual}° Fahrenheit para Kelvin: ${calculo.toFixed(2)}°K`);
+}else if (escalaAtual === 'K' && conversao === 'C'){
+    calculo = (temperaturaAtual * 1) - 273.15;
+    window.alert(`${temperaturaAtual}° Kelvin para Celsius: ${calculo.toFixed(2)}°C`);
+}else if (escalaAtual === 'K' && conversao === 'F'){
+    calculo = (temperaturaAtual * 1) * 9/5 - 459.67;
+    window.alert(`${temperaturaAtual}° Kelvin para Fahrenheit: ${calculo.toFixed(2)}°F`);
 }
