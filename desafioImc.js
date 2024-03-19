2511f404fb3d41b653493e4b5a6cb538bf966b79
Matheus Pedrosa <106934941+Omatheuspedrosa@users.noreply.github.com>
2024-03-19 23:27:25 +0000

    condicoes na calculadora de imc

--- desafioImc.js
+++ desafioImc.js
@@ -1,6 +1,29 @@
 function calcularImc() {
-    var pegarPeso = parseFloat(document.getElementById('peso').value); 
-    var pegarAltura = parseFloat(document.getElementById('altura').value); 
-    var calculo = pegarPeso / (pegarAltura ** 2);  
-    document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2); 
+    const pegarPeso = parseFloat(document.getElementById('peso').value); 
+    const pegarAltura = parseFloat(document.getElementById('altura').value); 
+    const calculo = pegarPeso / (pegarAltura ** 2);  
+    if (calculo < 18.5) {
+        document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2);
+        document.getElementById('categoria').innerHTML = "Abaixo do peso";
+    }else if (calculo <= 24.9) {
+        document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2);
+        document.getElementById('categoria').innerHTML = "Peso normal";
+    }
+    else if (calculo <= 29.9) {
+        document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2);
+        document.getElementById('categoria').innerHTML = "Sobrepeso";
+    }
+    else if (calculo <= 34.9) {
+        document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2);
+        document.getElementById('categoria').innerHTML = "Obesidade grau I";
+    }
+    else if (calculo <= 39.9) {
+        document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2);
+        document.getElementById('categoria').innerHTML = "Obesidade grau II";
+    }
+    else {
+        document.getElementById('resultado').innerHTML = "IMC: " + calculo.toFixed(2);
+        document.getElementById('categoria').innerHTML = "Obesidade grau III";
+    }
+     
 }
