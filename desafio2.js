795d29e7bd860101d61a6a608eb33d0847f9cada
Matheus <matheuspedrosa307@gmail.com>
2024-03-21 11:55:32 -0300

    desafio4

--- desafio2.js
+++ desafio2.js
@@ -1,24 +1,24 @@
-function calcularIMC() {
-const 
-altura = window.prompt("Digite sua altura:"),
-peso = window.prompt("Digite o seu peso:"),
-imc = peso / altura ** 2;
+function calcularIMC(peso, altura) {
+    const imc = peso / (altura ** 2);
+    let mensagem;
 
-if (imc < 18.5) {
-    window.alert(`IMC: ${imc.toFixed(2)} \nAbaixo do peso`)
-}else if (imc <= 24.9) {
-    window.alert(`IMC: ${imc.toFixed(2)} \nPeso normal`)
-}
-else if (imc <= 29.9) {
-    window.alert(`IMC: ${imc.toFixed(2)} \nSobrepeso`)
-}
-else if (imc <= 34.9) {
-    window.alert(`IMC: ${imc.toFixed(2)} \nObesidade grau I`)
-}
-else if (imc <= 39.9) {
-    window.alert(`IMC: ${imc.toFixed(2)} \nObesidade grau II`)
-}
-else {
-    window.alert(`IMC: ${imc.toFixed(2)} \nObesidade grau III`)
-}
+    if (imc < 18.5) {
+        mensagem = (`IMC: ${imc.toFixed(2)} \nAbaixo do peso`);
+    }else if (imc <= 24.9) {
+        mensagem = (`IMC: ${imc.toFixed(2)} \nPeso normal`);
+    }
+    else if (imc <= 29.9) {
+        mensagem = (`IMC: ${imc.toFixed(2)} \nSobrepeso`);
+    }
+    else if (imc <= 34.9) {
+        mensagem = (`IMC: ${imc.toFixed(2)} \nObesidade grau I`);
+    }
+    else if (imc <= 39.9) {
+        mensagem = (`IMC: ${imc.toFixed(2)} \nObesidade grau II`);
+    }
+    else {
+        mensagem = (`IMC: ${imc.toFixed(2)} \nObesidade grau III`);
+    }
+    
+    return mensagem;
 }
