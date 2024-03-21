9d72543ad97372e25c7ea29aa280d7fbbae55277
Matheus <matheuspedrosa307@gmail.com>
2024-03-21 09:04:53 -0300

    checklist e tratamentos de erros no desafio 3

--- desafio3.js
+++ desafio3.js
@@ -4,27 +4,30 @@ if (!isNaN(temperaturaAtual)) {
     if (escalaAtual === 'C' || escalaAtual === 'F' || escalaAtual === 'K') {
         const conversao = window.prompt('Escolha a escala a ser convertida: \nF - Fahrenheit \nC - Celsius \nK - Kelvin').toUpperCase();
         if (conversao === 'C' || conversao === 'F' || conversao === 'K') {
-            let calculo = 0;
-
-            if (escalaAtual === 'C' && conversao === 'F') {
-                calculo = (temperaturaAtual * 9/5) + 32;
-                window.alert(`${temperaturaAtual}° Celsius para Fahrenheit: ${calculo.toFixed(2)}°F`)
-            }else if (escalaAtual === 'C' && conversao === 'K') {
-                calculo = (temperaturaAtual * 1) + 273;
-                window.alert(`${temperaturaAtual}° Celsius para Kelvin: ${calculo.toFixed(2)}°K`);
-            }else if (escalaAtual === 'F' && conversao === 'C'){
-                calculo = (temperaturaAtual - 32) * (5/9);
-                window.alert(`${temperaturaAtual}° Fahrenheit para Celsius: ${calculo.toFixed(2)}°C`);
-            }else if (escalaAtual === 'F' && conversao === 'K'){
-                calculo = ((temperaturaAtual * 1) + 459.67) * 5/9;
-                window.alert(`${temperaturaAtual}° Fahrenheit para Kelvin: ${calculo.toFixed(2)}°K`);
-            }else if (escalaAtual === 'K' && conversao === 'C'){
-                calculo = (temperaturaAtual * 1) - 273.15;
-                window.alert(`${temperaturaAtual}° Kelvin para Celsius: ${calculo.toFixed(2)}°C`);
-            }else if (escalaAtual === 'K' && conversao === 'F'){
-                calculo = (temperaturaAtual * 1) * 9/5 - 459.67;
-                window.alert(`${temperaturaAtual}° Kelvin para Fahrenheit: ${calculo.toFixed(2)}°F`);
+            if(conversao != escalaAtual) {
+                if (escalaAtual === 'C' && conversao === 'F') {
+                    calculo = (temperaturaAtual * 9/5) + 32;
+                    window.alert(`${temperaturaAtual}° Celsius para Fahrenheit: ${calculo.toFixed(2)}°F`)
+                }else if (escalaAtual === 'C' && conversao === 'K') {
+                    calculo = (temperaturaAtual * 1) + 273;
+                    window.alert(`${temperaturaAtual}° Celsius para Kelvin: ${calculo.toFixed(2)}°K`);
+                }else if (escalaAtual === 'F' && conversao === 'C'){
+                    calculo = (temperaturaAtual - 32) * (5/9);
+                    window.alert(`${temperaturaAtual}° Fahrenheit para Celsius: ${calculo.toFixed(2)}°C`);
+                }else if (escalaAtual === 'F' && conversao === 'K'){
+                    calculo = ((temperaturaAtual * 1) + 459.67) * 5/9;
+                    window.alert(`${temperaturaAtual}° Fahrenheit para Kelvin: ${calculo.toFixed(2)}°K`);
+                }else if (escalaAtual === 'K' && conversao === 'C'){
+                    calculo = (temperaturaAtual * 1) - 273.15;
+                    window.alert(`${temperaturaAtual}° Kelvin para Celsius: ${calculo.toFixed(2)}°C`);
+                }else if (escalaAtual === 'K' && conversao === 'F'){
+                    calculo = (temperaturaAtual * 1) * 9/5 - 459.67;
+                    window.alert(`${temperaturaAtual}° Kelvin para Fahrenheit: ${calculo.toFixed(2)}°F`);
+                }
+            }else {
+                window.alert('A escala a ser convertida precisa ser diferente da escala atual! Recarregue a página e tente novamente.')
             }
+            
         }else {
             window.alert('Você digitou uma conversão inválida. Recarregue a página e tente novamente');
         }
