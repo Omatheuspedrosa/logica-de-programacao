43bebc20ad114bae4f8542761d978e247ba2572d
Theus <matheuspedrosa307@gmail.com>
2024-04-06 19:46:42 -0300

    correção no imc e estilização do exercicio de futebol

--- Desafios/desafio2.js
+++ Desafios/desafio2.js
@@ -1,23 +1,24 @@
 function calcularIMC(peso, altura) {
-    const imc = peso / (altura ** 2);
+    imc = peso / (altura ** 2);
 
-    if (imc < 18.5) {
-        mensagem = (`IMC: ${imc.toFixed(2)} \nAbaixo do peso`);
-    }else if (imc <= 24.9) {
-        mensagem = (`IMC: ${imc.toFixed(2)} \nPeso normal`);
+    if (isNaN(peso) || isNaN(altura)) {
+        return 'Utilize entradas válidas no peso e altura'
+    } else if (imc < 18.5) {
+        return (`IMC: ${imc.toFixed(2)} \nAbaixo do peso`);
+    } else if (imc <= 24.9) {
+        return (`IMC: ${imc.toFixed(2)} \nPeso normal`);
     }
     else if (imc <= 29.9) {
-        mensagem = (`IMC: ${imc.toFixed(2)} \nSobrepeso`);
+        return (`IMC: ${imc.toFixed(2)} \nSobrepeso`);
     }
     else if (imc <= 34.9) {
-        mensagem = (`IMC: ${imc.toFixed(2)} \nObesidade grau I`);
+        return (`IMC: ${imc.toFixed(2)} \nObesidade grau I`);
     }
     else if (imc <= 39.9) {
-        mensagem = (`IMC: ${imc.toFixed(2)} \nObesidade grau II`);
+        return (`IMC: ${imc.toFixed(2)} \nObesidade grau II`);
     }
     else {
-        mensagem = (`IMC: ${imc.toFixed(2)} \nObesidade grau III`);
+        return (`IMC: ${imc.toFixed(2)} \nObesidade grau III`);
     }
-    
-    return mensagem;
+
 }
