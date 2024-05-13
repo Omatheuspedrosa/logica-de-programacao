af89ab6d376364384179ae315ea1be150627b2eb
Theus <matheuspedrosa307@gmail.com>
2024-05-13 20:39:54 -0300

    chore: criar navbar com js

---
+++ js/navbar.js
@@ -0,0 +1,36 @@
+const ancoras = [
+    {"url": "index.html", "text": "Home"},
+    {"url": "/Exercícios/exe01.html", "text": "Classificar Triângulo"},
+    {"url": "/Exercícios/exe02.html", "text": "Descontos"},
+    {"url": "/Exercícios/exe03.html", "text": "Placar de Futebol"},
+    {"url": "/Exercícios/exe04.html", "text": "Busca de Nomes"},
+    {"url": "/Exercícios/exe05.html", "text": "Posto de Gasolina"},
+    {"url": "/Exercícios/exe06.html", "text": "Produtos com Desconto"},
+    {"url": "/Exercícios/exe07.html", "text": "Natalidade e Mortalidade"}
+];
+
+function criaAncora(url, texto) {
+
+    const a = document.createElement(a);
+    a.href = url;
+    a.innerHTML = texto;
+
+    return a;
+};
+
+function criaLi(ancora) {
+
+    const li = document.createElement(li);
+    li.appendChild(ancora);
+
+    return li;
+};
+
+function criaUl(ancoras) {
+
+    const ul = document.createElement(ul);
+    ancoras.forEach(ancora => {
+        
+    });
+
+};
